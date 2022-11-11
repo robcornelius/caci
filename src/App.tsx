fb8edd2102e64b8b1927c2de8b3c92bbfa0a5053
robcornelius <rob.cornelius@gmail.com>
2022-11-11 11:19:49 +0000

    basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./img/Star_Wars_Logo.svg";
+import cup from "./img/cup.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} alt="logo" height={300} />
+        <h1>Sample React project using the SWAPI API</h1>
+        <h2>
+          Results are filtered to starshits with a crew size less than 10 and
+          sorted by crew size
+        </h2>
+        <h3>
+          The starship that has featured in most filmes will show a{" "}
+          <img src={cup} alt="cup" height={40} />
+        </h3>
       </header>
     </div>
   );
