70d0f5a39d36fc93ba6a4489be47e68b135a35ed
robcornelius <rob.cornelius@gmail.com>
2022-11-12 11:15:39 +0000

    bibbety bobbity boo

---
+++ src/components/styled/LoadingSpinner.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const LoadingSpinner = styled.div`
+  height: 100vh;
+  width: 100vw;
+  & img {
+    position: absolute;
+    left: 50%;
+    bottom: 0px;
+  }
+`;
+export default LoadingSpinner;
